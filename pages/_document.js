ddca21046e589ba9b3e10fe2684e34d691176239
Farhanaz Rupaidha <38101740+farhanazrupaidha@users.noreply.github.com>
2023-11-07 09:46:05 +0700

    5th commit
    google analytic added

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ export default class MyDocument extends Document {
                 rel="stylesheet"
                 href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"
             />
-            <meta name="google-site-verification" content="0yodTROwuSO4D4xYd5WBRBnm7iehZRuZx_7shUU-OK8" />
+            <meta name="google-site-verification" content="Q8sQqkInqGJz4af1-uM1t3BbBWqsVaZjHQNKOapd-18" />
             <link
               rel="icon"
               href="/icon?<generated>"
@@ -24,6 +24,24 @@ export default class MyDocument extends Document {
               type="image/<generated>"
               sizes="<generated>"
             />
+            {/* Global Site Tag (gtag.js) - Google Analytics */}
+            <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+                />
+            <script
+                dangerouslySetInnerHTML={{
+                __html: `
+                 window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                page_path: window.location.pathname,
+                });
+                `,
+                }}
+            />
+            {/* Global Site Tag (gtag.js) - Google Analytics */}            
         </Head>
         <body>
           <Main />
