7ed565d971d5868a9e382b56071b56021ded6a01
Farhanaz Rupaidha <38101740+farhanazrupaidha@users.noreply.github.com>
2023-11-17 10:25:07 +0700

    5th commit

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://www.yayasanwangsakerta.org/',
+  siteUrl: process.env.SITE_URL || 'https://yayasanwangsakerta.org/',
   generateRobotsTxt: true, // (optional)
   sitemapSize: 7000
 }
