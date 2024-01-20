291f8f890142e947cf4be83333e7f5a709fea917
Farhanaz Rupaidha <38101740+farhanazrupaidha@users.noreply.github.com>
2024-01-20 20:16:20 +0700

    7th commit

---
+++ components/paginate.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+
+const Paginate = ({
+	postsPerPage,
+	totalPosts,
+	currentPage,
+	paginate,
+	previousPage,
+	nextPage,
+}) => {
+	const pageNumbers = [];
+
+	for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+		pageNumbers.push(i);
+	}
+
+return (
+<>
+    <Stack spacing={2} sx={{borderRadius: 5, m:5}} justifyContent="center" alignItems="center">
+			<ButtonGroup  variant="contained" sx={{borderRadius: 5}} color="secondary" backgroundColor="white" aria-label="medium secondary button group">
+				<Button onClick={previousPage} className="page-number">
+					Terbaru <KeyboardArrowLeftIcon />
+				</Button>
+				<Button onClick={nextPage} className="page-number">
+					Terdahulu <KeyboardArrowRightIcon />
+				</Button>
+			</ButtonGroup>
+	</Stack>
+    </>
+	);
+};
+
+export default Paginate;
+
--- pages/catatanlapangan.js
+++ pages/catatanlapangan.js
@@ -1,27 +1,108 @@
+import React, { useState, useEffect } from 'react';
+import { request } from 'graphql-request';
 import Head from 'next/head'
 import dynamic from 'next/dynamic'
 import { getAllPostsForHome } from '../lib/graphcms'
 
 const Container = dynamic(() => import('components/container'));
+const PostPreview = dynamic(() => import('components/post-preview'));
 const Layout = dynamic(() => import('components/layout'));
-const HeroPost = dynamic(() => import('components/hero-post'));
-const MoreStories = dynamic(() => import('components/more-stories'), {
+const Paginate = dynamic(() => import('components/paginate'), {
   ssr: false,
 });
 const Intro = dynamic(() => import('components/intro'), {
   ssr: false,
 });
+const HeroPost = dynamic(() => import('components/hero-post'));
 
+import Divider from '@mui/material/Divider';
+import Box from '@mui/material/Box';
 import Button from "@mui/material/Button";
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 export default function Index({ posts, preview }) {
+
+	const [blogPosts, setBlogPosts] = useState([]);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [totalPosts, setTotalPosts] = useState();
+	const [postsPerPage] = useState(10);
+
   const heroPost = posts[0]
   const morePosts = posts.slice(1)
-  return (
+  
+	useEffect(() => {
+		const fetchBlogPosts = async () => {
+			const { posts, postsConnection } = await request(
+				'https://api-ap-southeast-2.hygraph.com/v2/clknbgxdh0r7u01t62h320963/master',
+				`
+			{
+				posts (orderBy: date_DESC, first: ${postsPerPage}, skip: ${
+					currentPage * postsPerPage - postsPerPage
+				}) {
+					                  title
+                            slug
+                            excerpt
+                            date
+                            tags
+                            coverImage {
+                              url(transformation: {
+                                image: {
+                                  resize: {
+                                    fit:crop,
+                                    width:2000,
+                                    height:1000
+                                  }
+                                }
+                              })
+                            }
+                            author {
+                              name
+                            }
+                            seo {
+                                  title
+                                  description
+                                  keywords
+                                  image {
+                                    url
+                                  }
+                                }
+                          }
+				 postsConnection {
+					pageInfo {
+					  pageSize
+					}
+				 }
+			}
+		 `
+			);
+
+			setBlogPosts(posts);
+			setTotalPosts(postsConnection.pageInfo.pageSize);
+		};
+
+		fetchBlogPosts();
+	}, [currentPage, postsPerPage]);
+
+	const paginate = (pageNumber) => {
+		setCurrentPage(pageNumber);
+	};
+
+	const previousPage = () => {
+		if (currentPage !== 1) {
+			setCurrentPage(currentPage - 1);
+		}
+	};
+
+	const nextPage = () => {
+		if (currentPage !== Math.ceil(totalPosts / postsPerPage)) {
+			setCurrentPage(currentPage + 1);
+		}
+	};
+
+	return (
     <>
       <Layout preview={preview}>
-               <Head
+      <Head
                      defaultTitle="Yayasan Wangsakerta"
                    >
                      <title>Yayasan Wangsakerta</title>
@@ -41,9 +122,9 @@ export default function Index({ posts, preview }) {
                      <meta name="twitter:image:src" content="/images/Wangsakerta - 2.jpg" />
                     <link rel="icon" href="/favicon/logo-wangsakerta.png" />
                </Head>
-        <Container>
-          <Intro />
-          {heroPost && (
+        <Container> 
+        <Intro />   
+        {heroPost && (
             <HeroPost
               title={heroPost.title}
               coverImage={heroPost.coverImage}
@@ -52,14 +133,43 @@ export default function Index({ posts, preview }) {
               slug={heroPost.slug}
               excerpt={heroPost.excerpt}
             />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-          <Button variant="contained" endIcon={<ArrowForwardIcon />} href="/catatanlapangan-lama" color="secondary"  size="large" sx={{borderRadius: 5, mb:10}}>Arsip Catatan 2017 - 2021</Button>
+          )}                      
+			{blogPosts ? (
+			<Box>
+			<div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
+				{blogPosts.map((blogPost) => (
+                   <PostPreview
+                     key={blogPost.slug}
+                     title={blogPost.title}
+                     coverImage={blogPost.coverImage}
+                     date={blogPost.date}
+                     tags={blogPost.tags}
+                     author={blogPost.author}
+                     slug={blogPost.slug}
+                     excerpt={blogPost.excerpt}
+                   />
+                   ))}
+                 </div>
+                 <center>
+                 <Paginate
+                 postsPerPage={postsPerPage}
+                   totalPosts={totalPosts}
+                   currentPage={currentPage}
+                   paginate={paginate}
+                   previousPage={previousPage}
+                   nextPage={nextPage}
+                  />
+               </center>
+       </Box>       
+			) : (
+				<div className="loading">Loading...</div>
+			)}
+      <Button variant="contained" endIcon={<ArrowForwardIcon />} href="/catatanlapangan-lama" color="secondary"  size="large" sx={{borderRadius: 5, mb:10}}>Arsip Catatan 2017 - 2021</Button>
         </Container>
       </Layout>
     </>
-  )
-}
+)
+};
 
 export async function getStaticProps({ preview = false }) {
   const posts = (await getAllPostsForHome(preview)) || []
