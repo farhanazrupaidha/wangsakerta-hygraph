f61e958de98ec91d4fe7cd679478b6d0a6a1ba47
Farhanaz Rupaidha <38101740+farhanazrupaidha@users.noreply.github.com>
2025-05-01 20:14:11 +0200

    update next.config,js
    12th commit

--- next.config.js
+++ next.config.js
@@ -11,14 +11,7 @@ module.exports = withBundleAnalyzer({
   },
    reactStrictMode: true,
    images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'media.graphassets.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+    domains: ['ap-southeast-2.graphassets.com'],
     formats: ['image/avif', 'image/webp'],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
